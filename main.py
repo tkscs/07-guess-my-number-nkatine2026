a5eea8210cebbc07df913a74810a4c09a1d477dc
nkatine2026 <nkatine2026@kehillahstudent.org>
2025-11-06 09:13:16 -0800

    done

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 # this program will guess the number
 # the user will say "yes" or "higher" or "lower"
 # the program will ask to play again or exit
+i = 0
+while True:
+    w = input(f"is your number {i}?")
+    if w == "lower":
+        i = i-1
+    if w == "higher":
+        i = i+1
+    if w == "yes": break 
+print("yay")
+         
